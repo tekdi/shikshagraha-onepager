7e4989237998bdc62e7b9c5f2c92ecc755e998a4
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2024-12-22 06:00:02 +0530

    Added logic of roles and sub role

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {  FormsModule, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -12,18 +13,38 @@ import { CommonModule } from '@angular/common';
 export class RegistrationComponent {
   registrationForm;
   passwordVisible: boolean = false;
-  
+  isSubRoleEnabled: boolean = false;
+
   constructor(private fb: FormBuilder) {
     this.registrationForm = this.fb.group({
       name: ['', [Validators.required]],
       dob: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],
-      udise: ['', [Validators.required]]
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
+        ],
+      ],
+      userRole: ['', [Validators.required]],
+      subUserRole: [{ value: '', disabled: true }, [Validators.required]],
+      udise: ['', [Validators.required]],
     });
   }
 
   onSubmit() {
-    console.log('Form submitted:', this.registrationForm);
+    console.log('Form submitted:', this.registrationForm.value);
+  }
+
+  onUserRoleChange() {
+    const userRole = this.registrationForm.get('userRole')?.value;
+    if (userRole === 'HT & Official') {
+      this.registrationForm.get('subUserRole')?.enable(); // Enable the dropdown
+    } else {
+      this.registrationForm.get('subUserRole')?.disable(); // Disable the dropdown
+      this.registrationForm.get('subUserRole')?.reset(); // Reset the value
+    }
   }
 }
