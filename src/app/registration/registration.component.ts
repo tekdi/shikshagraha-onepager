27a8907fd31f9487de32073e4bc4146fb9d502b1
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2024-12-22 05:42:30 +0530

    Added logic to show and hide password

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -11,6 +11,7 @@ import { CommonModule } from '@angular/common';
 })
 export class RegistrationComponent {
   registrationForm;
+  passwordVisible: boolean = false;
   
   constructor(private fb: FormBuilder) {
     this.registrationForm = this.fb.group({
