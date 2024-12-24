b037b7ce49817989d24522ab330b1fadbe0176fd
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2024-12-24 10:43:01 +0530

    End points constant file

--- src/app/service/urlConstants.ts
+++ src/app/service/urlConstants.ts
@@ -1,5 +1,6 @@
 export const urlConstants = {
     API_URLS: {
-    SEARCH_LOCATION:'https://dev.sunbirdsaas.com/api/data/v1/location/search'
+    SEARCH_LOCATION: 'https://dev.sunbirdsaas.com/api/data/v1/location/search',
+    SUBMIT_USER_DATA: 'https://dev.sunbirdsaas.com/one/api/user/registration'
   }
 }
