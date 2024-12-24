9c2433ab978a4f13f14a970b744e9d5f93cd313a
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2024-12-24 16:11:42 +0530

    added: url constants

--- src/app/service/urlConstants.ts
+++ src/app/service/urlConstants.ts
@@ -2,6 +2,7 @@ export const urlConstants = {
     API_URLS: {
     SEARCH_LOCATION: 'https://dev.sunbirdsaas.com/api/data/v1/location/search',
     SUBMIT_USER_DATA: 'https://dev.sunbirdsaas.com/one/api/user/registration',
-    OTP_GENERATE: 'https://sunbirdsaas.com/api/otp/v2/generate'
+    OTP_GENERATE: 'https://sunbirdsaas.com/api/otp/v2/generate',
+    OTP_VERIFY: 'https://sunbirdsaas.com/api/otp/v2/verify'
   }
 }
