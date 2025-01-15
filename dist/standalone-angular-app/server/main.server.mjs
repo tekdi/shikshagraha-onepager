6e55727cd4290f41cf1b9a61795a6b21e6ef9860
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2025-01-15 09:34:46 +0530

    pushed :: dist build

--- dist/standalone-angular-app/server/main.server.mjs
+++ dist/standalone-angular-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MR27W3MJ.mjs";import"./chunk-4WPPJDLU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-SKDUAZ3Z.mjs";import"./chunk-4WPPJDLU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
