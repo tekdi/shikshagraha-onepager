c6c474738782ed3ddbc84779711a427d1899ed45
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2025-01-28 16:02:23 +0530

    Fixed ::  Password issue

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -114,7 +114,7 @@ export class RegistrationComponent {
           Validators.required,
           Validators.minLength(8),
           Validators.pattern(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/
           ),
         ],
       ],
