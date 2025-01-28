3da1976efbf8ff9af2c181d78b84f0baa7849d41
nidhikachhawa24 <nidhi_k@tekditechnologies.com>
2025-01-28 12:25:22 +0530

    added :: dist folder

--- dist/standalone-angular-app/server/main.server.mjs
+++ dist/standalone-angular-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SX2ISQRO.mjs";import"./chunk-TAS5KT7J.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IOAAEEIP.mjs";import"./chunk-TAS5KT7J.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
